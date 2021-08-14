841b457d258acb112eb172a94fb81d74d29c4d3d
noah <ssh7636@gmail.com>
2021-08-14 13:40:47 +0900

    https://leetcode.com/problems/can-place-flowers/

---
+++ src/leetcode/Can_Place_Flowers_605.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class Can_Place_Flowers_605 {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for(int i = 0; i < flowerbed.length; i++){
+            int left = (i - 1) < 0 ? 0 : flowerbed[i-1];
+            int right = (i + 1) >= flowerbed.length ? 0 : flowerbed[i+1];
+            int current = flowerbed[i];
+
+            if(left == 0 && current == 0 && right == 0){
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+        return n > 0 ? false : true;
+    }
+}
