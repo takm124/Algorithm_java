7209de025ddc1819cf49a906a240b1ffeb5e48eb
noah <ssh7636@gmail.com>
2021-08-17 10:43:31 +0900

    https://leetcode.com/problems/sign-of-the-product-of-an-array/

---
+++ src/leetcode/Sign_of_the_Product_of_an_Array_1822.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class Sign_of_the_Product_of_an_Array_1822 {
+    public int arraySign(int[] nums) {
+        int count = 0;
+
+        for (int num : nums) {
+            if (num == 0) return 0;
+
+            if (num < 0) {
+                count++;
+            }
+        }
+
+        return (count % 2) == 0 ? 1 : -1;
+    }
+}
