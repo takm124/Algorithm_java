e192ecf952e64f2b45f7ce11f2b62f2167f087e8
noah <ssh7636@gmail.com>
2021-08-23 19:22:19 +0900

    https://leetcode.com/problems/water-bottles/

---
+++ src/leetcode/Water_Bottles_1518.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class Water_Bottles_1518 {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int answer = numBottles;
+
+        while (numBottles >= numExchange) {
+            int rem = numBottles % numExchange;
+            numBottles /= numExchange;
+            answer += numBottles;
+            numBottles += rem;
+        }
+
+        return answer;
+    }
+}
