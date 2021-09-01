a55a265db79f6119a0608174c3eec5f727912477
noah <ssh7636@gmail.com>
2021-09-01 23:14:59 +0900

    https://leetcode.com/problems/calculate-money-in-leetcode-bank/submissions/

---
+++ src/leetcode/Calculate_Money_in_Leetcode_Bank_1716.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+public class Calculate_Money_in_Leetcode_Bank_1716 {
+    public int totalMoney(int n) {
+        int count = 1;
+        int answer = 0;
+
+        while (n > 0) {
+            if (n >= 7) {
+                for (int i = 0; i < 7; i++) {
+                    answer += (count + i);
+                }
+            }
+
+            if (n < 7) {
+                for (int i = 0; i < n; i++) {
+                    answer += (count + i);
+                }
+            }
+
+            n -= 7;
+            count++;
+
+        }
+        return answer;
+    }
+}
