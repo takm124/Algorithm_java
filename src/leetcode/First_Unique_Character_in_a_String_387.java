23d779a05b1419fa3563f644e79f51da218abd58
noah <ssh7636@gmail.com>
2021-09-08 21:16:17 +0900

    https://leetcode.com/problems/first-unique-character-in-a-string/

---
+++ src/leetcode/First_Unique_Character_in_a_String_387.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class First_Unique_Character_in_a_String_387 {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            if (!map.containsKey(c)) {
+                map.put(c, 1);
+            } else {
+                map.put(c, map.get(c) + 1);
+            }
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+
+}
