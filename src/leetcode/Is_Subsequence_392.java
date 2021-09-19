ef607e3b4bbca3950ec3829795c79122fb21b6de
noah <ssh7636@gmail.com>
2021-09-19 14:00:16 +0900

    https://leetcode.com/problems/is-subsequence/

---
+++ src/leetcode/Is_Subsequence_392.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class Is_Subsequence_392 {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0;
+        int j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    }
+}
