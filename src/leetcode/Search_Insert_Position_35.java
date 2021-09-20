92b9784b4c5945ede1299de5e54b5d3091bddb80
noah <ssh7636@gmail.com>
2021-09-20 14:16:03 +0900

    https://leetcode.com/problems/search-insert-position/

---
+++ src/leetcode/Search_Insert_Position_35.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class Search_Insert_Position_35 {
+    public int searchInsert(int[] nums, int target) {
+        int lt = 0;
+        int rt = nums.length - 1;
+
+        while (lt <= rt) {
+            int center = (lt + rt) / 2;
+            if (nums[center] == target) {
+                return center;
+            } else if (nums[center] > target) {
+                rt = center - 1;
+            } else {
+                lt = center + 1;
+            }
+        }
+
+        return lt;
+    }
+}
