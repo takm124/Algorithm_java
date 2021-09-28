83638b16f9235dc8b90eced19c07dab146da27df
noah <ssh7636@gmail.com>
2021-09-28 21:45:20 +0900

    https://leetcode.com/problems/power-of-three/

---
+++ src/leetcode/Power_of_Three_326.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class Power_of_Three_326 {
+    public boolean isPowerOfThree(int n) {
+        if (n < 1) return false;
+
+        while (n > 1) {
+            if (n % 3 != 0) {
+                return false;
+            }
+            n = n / 3;
+        }
+
+        return true;
+    }
+}
