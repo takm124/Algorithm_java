110dde21a70585b3bc772c7b5e70a4ea11080e01
noah <ssh7636@gmail.com>
2021-11-21 23:22:43 +0900

    https://leetcode.com/problems/find-lucky-integer-in-an-array/

---
+++ src/leetcode/Find_Lucky_Integer_in_an_Array_1394.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.*;
+
+
+public class Find_Lucky_Integer_in_an_Array_1394 {
+    public int findLucky(int[] arr) {
+        int[] nums = new int[501];
+
+        for (int num : arr) {
+            nums[num]++;
+        }
+
+        int answer = -1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == i) {
+                answer = Math.max(answer, i);
+            }
+        }
+
+        return answer;
+    }
+}
