e87cae4dc6e4245ddf4425a40395f10b81059c75
Seongho-Shin <sim2308@naver.com>
2022-11-21 10:27:27 +0900

    최솟값 만들기

---
+++ src/programmers/Question004.java
@@ -0,0 +1,19 @@
+package programmers;
+
+import java.util.*;
+
+public class Question004 { // 최솟값 만들기
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for (int i = 0; i < A.length; i++) {
+            answer += (A[i] * B[A.length - 1 - i]);
+        }
+
+        return answer;
+    }
+}
