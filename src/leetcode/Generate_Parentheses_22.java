fd7899dd38d6e1370e567bc84a6e82b751dcbf7f
Seongho-Shin <sim2308@naver.com>
2022-12-12 09:48:03 +0900

    https://leetcode.com/problems/generate-parentheses/

---
+++ src/leetcode/Generate_Parentheses_22.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generate_Parentheses_22 {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        recur(ans, "(", n-1, n); // answer, start, open, end
+
+        return ans;
+    }
+
+    public void recur(List<String> ans, String result, int open, int end) {
+        if (open == 0 && end == 0) {
+            ans.add(result);
+            return;
+        }
+
+        if (end > open) {
+            recur(ans, result += ')', open, end-1);
+        }
+
+        if (open > 0) {
+            recur(ans, result += '(', open-1, end);
+        }
+    }
+
+}
